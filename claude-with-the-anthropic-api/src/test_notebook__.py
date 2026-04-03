9f16405dff8f9e61d7d9707266331c8143b7576d
rohit dhankar <dhankar.rohit@gmail.com>
2026-04-03 19:39:44 +0530

    init-certs--bug-1

--- claude-with-the-anthropic-api/src/test_notebook__.py
+++ claude-with-the-anthropic-api/src/test_notebook__.py
@@ -7,32 +7,67 @@ from anthropic import Anthropic
 client = Anthropic()
 model = "claude-sonnet-4-0"
 
-def add_user_message(messages, text):
+ls_messages = []
+
+def add_user_message(ls_messages, text):
     user_message = {"role": "user", "content": text}
-    messages.append(user_message)
+    ls_messages.append(user_message)
 
-def add_assistant_message(messages, text):
+def add_assistant_message(ls_messages, text):
     assistant_message = {"role": "assistant", "content": text}
-    messages.append(assistant_message)
+    ls_messages.append(assistant_message)
 
-# Make a request
-def chat(messages):
+
+def chat(ls_messages):
     """ 
     """
+    print("---FROM--CHAT-----print(type(ls_messages))-------")
+    print(type(ls_messages))
+    print("  -- "*100)
+
     message = client.messages.create(
         model=model,
         max_tokens=1000,
-        messages=[
-            {
-                "role": "user",
-                "content":  "hello- whats the sum of -- 6677 + 8899 "
-            }
-            
-        ]
+        messages=ls_messages
     )
 
+    print("---FROM--CHAT------------")
     print(message)
+    print("  "*100)
     print(message.content[0].text)
+    print("  "*100)
+    return message
+
+
+new_message_text = "Hello whats the sum of -- 667700 + 448899 "
+add_user_message(ls_messages,new_message_text)
+
+answer = chat(ls_messages)
+print("---FROM--module---answer--------")
+print(answer)
+print("  "*100)
+print("---FROM--module---type(answer))--------")
+print(type(answer)) ## <class 'anthropic.types.message.Message'>
+print("  "*100)
+print("---FROM--module---type(answer))--------")
+answer_text = answer.content[0].text
+print(type(answer_text))
+print("  "*100)
+
+add_assistant_message(ls_messages,answer_text) #answer.content[0].text
+
+
+# Add another New random sentence
+new_message_text_random = "Add another sentence "
+add_user_message(ls_messages,new_message_text_random)
+
+#Answer 2 Now 
+answer_2 = chat(ls_messages)
+print("---FROM--module---answer_2--------")
+print(answer_2)
+print("  "*100)
+
+
 
 """ 
 python test_notebook__.py 
@@ -54,6 +89,14 @@ server_tool_use=None,
 service_tier='standard'), stop_details=None)
 
 (env_overlander) dhankar@dhankar-1:~/.../claude_certs$ 
+
+[
+            {
+                "role": "user",
+                "content":  "hello- whats the sum of -- 6677 + 8899 "
+            }
+            
+        ]
 
 
 """
